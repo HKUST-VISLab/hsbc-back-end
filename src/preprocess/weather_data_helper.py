612e79d6043848b4c1164c51df1d774ce5ec8f73
Ming Yao <yaoming.thu@gmail.com>
2016-11-13 18:36:27 +0800

    Update comments

--- src/preprocess/weather_data_helper.py
+++ src/preprocess/weather_data_helper.py
@@ -24,36 +24,34 @@ ERROR_FETCH = 1
 SUCCESS = 0
 
 # logger config in this module
-_log_format = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
-_logger = logging.getLogger(__name__)
-_logger.setLevel(logging.INFO)
+_logger = utils.Logger(__name__)
 _db_config = utils.parse_json_file(DB_CONFIG_DIR)
 _station_config = utils.parse_json_file(STATION_CONFIG_DIR)
 _full_station_config = utils.parse_json_file(FULL_STATION_CONFIG_DIR)
 _forecast_config = utils.parse_json_file(FORECAST_STATION_CONFIG_DIR)
 
 
-def log_to_file(filename=None, level=logging.INFO):
-    """
-    Set log file to filename
-    :param filename: log file name
-    :param level: logging level
-    :return: None
-    """
-    _logger.setLevel(level)
-    if filename is None:
-        handler = logging.FileHandler('weather.log', 'w')
-        handler.setFormatter(_log_format)
-        _logger.addHandler(handler)
-    else:
-        handler = logging.FileHandler(filename, 'w')
-        handler.setFormatter(_log_format)
-        _logger.addHandler(handler)
+# def log_to_file(filename=None, level=logging.INFO):
+#     """
+#     Set log file to filename
+#     :param filename: log file name
+#     :param level: logging level
+#     :return: None
+#     """
+#     _logger.setLevel(level)
+#     if filename is None:
+#         handler = logging.FileHandler('weather.log', 'w')
+#         handler.setFormatter(_log_format)
+#         _logger.addHandler(handler)
+#     else:
+#         handler = logging.FileHandler(filename, 'w')
+#         handler.setFormatter(_log_format)
+#         _logger.addHandler(handler)
 
 
 def fetch_weather_data_of_site(grid_id):
     """
-    Fetch weather data of a site by the site's grid_id from hko
+    Deprecated. Fetch weather data of a site by the site's grid_id from hko
     :param grid_id: grid id of a station, you can find the grid id in the station_config.json file
     :return:
         a dict containing weather data, if return a string, it means an error occurs when requesting data
@@ -84,7 +82,7 @@ def fetch_weather_data_of_site(grid_id):
 
 def fetch_weather_data():
     """
-    Fetch weather data for all stations
+    Deprecated. Fetch weather data for all stations
     :return: a list of all fetched data, elements could be error code string or data dict
     """
     stations = _station_config['service_content']['station_details']
@@ -140,6 +138,10 @@ def fetch_forecast_data():
 
 
 def reformatting_raw_weather_data(raw_data):
+    """
+    Deprecated. Reformatting the data fetched by fetch_weather_data()
+    :return: a list of reformatted data
+    """
     formatted_data = []
     for raw in raw_data:
         rgdata = raw['RegionalWeather']
@@ -171,21 +173,26 @@ def reformatting_raw_weather_data(raw_data):
 
 
 def fetch_and_store_weather_data(forecast=False):
+    """
+    Fetch and store weather data from the maps.weather.gov.hk
+    :param forecast: indicating forecast or not
+    :return: 1 if error in fetching data, 0 if function succeed
+    """
     db_handler = _create_db_handler()
     collections = _db_config['weather_db']['collections']
     if not forecast:
         current = db_handler.get_collection(collections['current'])
         data_list = fetch_full_weather_data()
         if isinstance(data_list, str):
-            return 1
+            return ERROR_FETCH
         for data in data_list:
             current.replace_one({'stn': data['stn'], 'time': data['time']}, data, upsert=True)
-    return 0
+    return SUCCESS
 
 
 def fetch_full_weather_data():
     """
-    Fectch weather data through another source
+    Fectch weather data through another source from maps.weather.gov.hk
     :return:  a list of all fetched data, elements could be error code string or data dict
     """
     stations = _full_station_config['Stations']
@@ -218,11 +225,22 @@ def fetch_full_weather_data():
     return data
 
 
-def create_config():
+def create_statation_collection():
+    """
+    Create a collection containing configuration information for stations
+    :return:
+    """
     db_handler = _create_db_handler()
+    collection = db_handler.get_collection(_db_config['weather_db']['collections']['station'])
+    for station in _full_station_config['Stations']:
+        collection.replace_one(station, station, True)
 
 
 def _create_db_handler():
+    """
+    Create a database handler
+    :return: a handler for the database
+    """
     weather_db = _db_config['weather_db']
     return mongodb.MongoDB(weather_db['db_name'], weather_db['host'], weather_db['port'])
 
@@ -233,4 +251,4 @@ if __name__ == '__main__':
     # # fecth_weather_data_of_site(2207)
     # data_list = fetch_weather_data()
     # print(a)
-    fetch_and_store_weather_data()
+    create_statation_collection()
