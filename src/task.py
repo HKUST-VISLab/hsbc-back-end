acd1b369fbe480a4f5388f245c9c0f5d0b1fec3e
qmShen <joyshen06@gmail.com>
2017-05-29 20:07:24 +0800

    add tsm tasks

--- src/task.py
+++ src/task.py
@@ -4,9 +4,11 @@ Tasks for preprocessing
 
 from src.weather_AQI_fetcher.weather_data_helper import WeatherFetcher as WF
 from src.weather_AQI_fetcher.air_quality_fetch_helper import fetch_and_store_air_quality
+from src.tsm_fetcher.tsm_fetcher_helper import TSMFetcher as TF
 from src.utils import Logger, task_thread
 import time
 
+TSM_INTERVAL = 2 * 60
 CURRENT_INTERVAL = 200
 FORECAST_INTERVAL = 3600*3
 AQ_INTERVAL = 1200
@@ -14,13 +16,15 @@ AQ_INTERVAL = 1200
 TOTAL_RUNNING_TIME = 3600*24*7
 
 if __name__ == '__main__':
+    tsm = TF()
+
     tasks = []
     tasks.append(task_thread(WF.fetch_and_store_weather_data, CURRENT_INTERVAL, TOTAL_RUNNING_TIME, False))
     tasks.append(task_thread(WF.fetch_and_store_weather_data, FORECAST_INTERVAL, TOTAL_RUNNING_TIME, True))
+    tasks.append(task_thread(tsm.fetch_and_store, TSM_INTERVAL, TOTAL_RUNNING_TIME, False))
     tasks.append(task_thread(fetch_and_store_air_quality, AQ_INTERVAL, TOTAL_RUNNING_TIME ))
     for task in tasks:
         task.start()
     time.sleep(TOTAL_RUNNING_TIME)
     for task in tasks:
         task.join(0.1)
-
--- src/tsm_fetcher/tsm_fetcher_helper.py
+++ src/tsm_fetcher/tsm_fetcher_helper.py
@@ -35,10 +35,10 @@ class TSMFetcher:
             print('URLError = ' + data + '. Air Quality AQExtractor!')
         else:
             self.page = response.read()
-            return self.pares_xml_once()
+            return self.fetch_tsm_once()
 
 
-    def pares_xml_once(self):
+    def fetch_tsm_once(self):
         # Do we need to save the xml into local file?
         current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         def parse_tag(raw_tag):
@@ -58,23 +58,74 @@ class TSMFetcher:
             r_time = time.strptime(record['CAPTURE_DATE'.lower()], "%Y-%m-%dT%H:%M:%S")
             record['CAPTURE_DATE'.lower()] = time.strftime("%Y-%m-%d %H:%M:%S", r_time)
             records.append(record)
-
         return records
-    def store_tsm_data(self, records):
-        from pymongo import MongoClient
 
+    def store_tsm_data(self, records):
+        """
+        Store the records into the database
+        :param records:
+        :return:
+        """
+        from pymongo import MongoClient
         client = MongoClient('127.0.0.1', 27017)
         db = client['traffic']
         collection = db['traffic_speed']
         collection.insert_many(records)
         client.close()
 
+    def fetch_recent_records(self):
+        """
+        Collection the recent records(according to fetch time,
+        :return:
+        """
+        from pymongo import MongoClient
+        client = MongoClient('127.0.0.1', 27017)
+        db = client['traffic']
+        collection = db['traffic_speed']
+        group = {"$group": {"_id": "$fetch_time", "records": {"$push":"$$ROOT"},  "count": {"$sum": 1}}}
+        sort = {"$sort": {"_id": -1}}
+        limit = {"$limit": 1}
+        agg_list = list(collection.aggregate([group, sort, limit]))
+        if len(agg_list) == 0:
+            print("No record found!")
+            return[]
+        client.close()
+        return agg_list[0]['records']
 
-    def fetch_and_store(self):
+
+    def fetch_and_store(self, arg = None):
         records = self.fetch_TSM_data()
-        print('record', records)
+        old_records = self.fetch_recent_records()
+        if self.time_cover(records, old_records):
+            print('cover')
+            return
         self.store_tsm_data(records)
+
+    def time_cover(self, records1, records2):
+        """
+        Decides if two records are overalpped, if overalpped return true and will not insert data
+        :param records1: first records,
+        :param records2: second records
+        :return: True if overlapped(no insert), False if not overlapped(insert)
+        """
+
+        time_list1 = list(set([time.mktime(time.strptime(r['capture_date'], "%Y-%m-%d %H:%M:%S")) for r in records1]))
+        time_list1 = sorted(time_list1, key=lambda x: x, reverse=False)
+        time_list2 = list(set([time.mktime(time.strptime(r['capture_date'], "%Y-%m-%d %H:%M:%S")) for r in records2]))
+        time_list2 = sorted(time_list2, key=lambda x: x, reverse=False)
+
+        if len(time_list2) == 0 or len(time_list2) == 0:
+            return False
+        [s1, l1] = [time_list1[0], time_list1[-1]]
+        [s2, l2] = [time_list2[0], time_list2[-1]]
+        print([s1, l1], [s2, l2])
+        return False if s1>l2 or s2>l1 else True
+
 
 if __name__ == '__main__':
     tsm_fetcher = TSMFetcher()
     tsm_fetcher.fetch_and_store()
+    # records = tsm_fetcher.fetch_recent_records()
+    # records2 = tsm_fetcher.fetch_TSM_data()
+    # result = tsm_fetcher.time_cover(records, records2)
+    # print(result)
