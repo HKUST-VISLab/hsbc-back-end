c35496594ab11dd47e6887c582555a91d4a3cba4
qmShen <joyshen06@gmail.com>
2017-05-29 14:07:11 +0800

    's'

--- src/tsm_fetcher/tsm_fetcher_helper.py
+++ src/tsm_fetcher/tsm_fetcher_helper.py
@@ -7,6 +7,9 @@ import time
 
 REQUESTPATH = 'http://resource.data.one.gov.hk/td/speedmap.xml'
 
+tag_map = {
+    "LINK_ID": 'id',
+}
 class TSMFetcher:
     """This is a class used to fetch data from the hk gov data.
     default url = http://resource.data.one.gov.hk/td/speedmap.xml
@@ -32,7 +35,7 @@ class TSMFetcher:
             print('URLError = ' + data + '. Air Quality AQExtractor!')
         else:
             self.page = response.read()
-            self.pares_xml_once()
+            return self.pares_xml_once()
 
 
     def pares_xml_once(self):
@@ -46,23 +49,32 @@ class TSMFetcher:
         for action, element in parser.read_events():
             if parse_tag(element.tag) != self.entity_tag:
                 continue
-            segs = [{parse_tag(t.tag): t.text} for t in element]
+            segs = [{parse_tag(t.tag.lower()): t.text} for t in element]
             record = {}
             for seg in segs:
                 record.update(seg)
             record['fetch_time'] = current_time
             # The time is record['CAPTURE_DATE'], to be revised
-            r_time = time.strptime(record['CAPTURE_DATE'], "%Y-%m-%dT%H:%M:%S")
-            record['CAPTURE_DATE'] = time.strftime("%Y-%m-%d %H:%M:%S", r_time)
+            r_time = time.strptime(record['CAPTURE_DATE'.lower()], "%Y-%m-%dT%H:%M:%S")
+            record['CAPTURE_DATE'.lower()] = time.strftime("%Y-%m-%d %H:%M:%S", r_time)
             records.append(record)
 
         return records
+    def store_tsm_data(self, records):
+        from pymongo import MongoClient
 
-# 2017-05-29T00:14:18
+        client = MongoClient('127.0.0.1', 27017)
+        db = client['traffic']
+        collection = db['traffic_speed']
+        collection.insert_many(records)
+        client.close()
 
 
-
+    def fetch_and_store(self):
+        records = self.fetch_TSM_data()
+        print('record', records)
+        self.store_tsm_data(records)
 
 if __name__ == '__main__':
     tsm_fetcher = TSMFetcher()
-    tsm_fetcher.fetch_TSM_data()
+    tsm_fetcher.fetch_and_store()
