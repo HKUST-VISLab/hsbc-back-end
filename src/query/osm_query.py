0b50d82823279926c325af7f4f2069c35ea1d6e5
Wenchao <lemonz@outlook.com>
2018-05-24 23:34:53 +0800

    Store the osm_links from MongDB to PostGIS. Add fucntion to query near links with their length, distance and traffic condition.

--- src/query/osm_query.py
+++ src/query/osm_query.py
@@ -23,12 +23,11 @@ class OSMQuery:
     def query_point_distance_from_road(self, point_longitude, point_latitude, way_id, print_sql=False):
         """
         Get the nearest distance between a point and a road
-        :param
-        point_latitude: the latitude of a query point
-        point_longitude: the longitude of a query point
-        way_id: the way_id(osm_id) of the road
-        dict_cursor: if use dict data format for the query result
-        print_sql: if print the sql code
+        :param point_latitude: the latitude of a query point
+        :param point_longitude: the longitude of a query point
+        :param way_id: the way_id(osm_id) of the road
+        :param dict_cursor: if use dict data format for the query result
+        :param print_sql: if print the sql code
         :return: the nearest distance between a point a the road or None
         """
         try:
@@ -38,7 +37,7 @@ class OSMQuery:
         except psycopg2.OperationalError as e:
             print("dbname=" + self.database + ", host=" + self.host + ", port=" + self.port
                   + " , user=" + self.user + ", password=" + self.password)
-            print("OperationalError: " + e)
+            print("OperationalError: " + str(e))
         else:
             # Create a cursor object using dict output format
             cursor = postgresql_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -57,7 +56,7 @@ class OSMQuery:
             try:
                 cursor.execute(sql_query, (str(point_longitude), str(point_latitude), str(way_id)))
             except psycopg2.DatabaseError as e:
-                print("DatabaseError: " + e)
+                print("DatabaseError: " + str(e))
 
             result = cursor.fetchone()
             cursor.close()
@@ -68,14 +67,12 @@ class OSMQuery:
 
     def query_road_list_from_point(self, point_longitude, point_latitude, distance, print_sql=False):
         """
-        Get the nearest distance between a point and a road
-        :param
-        point_latitude: the latitude of a query point
-        point_longitude: the longitude of a query point
-        distance: search roads within a distance (unit: meter)
-        dict_cursor: if use dict data format for the query result
-        print_sql: if print the sql code
-        :return: None
+        Get the list of roads from a point within a distance
+        :param point_latitude: the latitude of a query point
+        :param point_longitude: the longitude of a query point
+        :param distance: search roads within a distance (unit: meter)
+        :param print_sql: if print the sql code
+        :return: road results in DictRow format
         """
         try:
             # Create connection to ices database
@@ -84,9 +81,9 @@ class OSMQuery:
         except psycopg2.OperationalError as e:
             print("dbname=" + self.database + ", host=" + self.host + ", port=" + self.port
                   + " , user=" + self.user + ", password=" + self.password)
-            print("OperationalError: " + e)
+            print("OperationalError: " + str(e))
         else:
-            # Create a cursor object using dict output format
+            # Create a cursor object using dict DictRow format
             cursor = postgresql_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
             sql_query = """
@@ -106,44 +103,114 @@ class OSMQuery:
             try:
                 cursor.execute(sql_query, (str(point_longitude), str(point_latitude), str(distance)))
             except psycopg2.DatabaseError as e:
-                print("DatabaseError: " + e)
+                print("DatabaseError: " + str(e))
+            else:
+                result = cursor.fetchall()
+                cursor.close()
+                postgresql_connection.close()
+                # Return the distance
+                return result
 
-            result = cursor.fetchall()
-            cursor.close()
-            postgresql_connection.close()
+    def query_osm_link_list_from_point(self, point_longitude, point_latitude, distance, print_sql=False):
+        """
+        Get the list of osm links from a point within a distance
+        :param point_latitude: the latitude of a query point
+        :param point_longitude: the longitude of a query point
+        :param distance: search roads within a distance (unit: meter)
+        :param print_sql: if print the sql code
+        :return: osm link results in DictRow format
+        """
+        try:
+            # Create connection to ices database
+            postgresql_connection = psycopg2.connect(dbname=self.database, host=self.host, port=self.port,
+                                                     user=self.user, password=self.password)
+        except psycopg2.OperationalError as e:
+            print("dbname=" + self.database + ", host=" + self.host + ", port=" + self.port
+                  + " , user=" + self.user + ", password=" + self.password)
+            print("OperationalError: " + str(e))
+        else:
+            # Create a cursor object using DictRow output format
+            cursor = postgresql_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-            # Return the distance
-            return result
+            sql_query = """
+            SELECT * FROM
+            (SELECT link_id, parent_id, ST_Length(ST_Transform(link,4326), false), 
+            ST_Distance_Sphere(ST_ClosestPoint(ST_Transform(l.link,4326), point.geom), point.geom)
+            FROM hk_osm_link l,
+            (SELECT ST_SetSRID(ST_MakePoint(%s, %s),4326) as geom) point
+            ORDER BY parent_id, st_distance_sphere ASC) AS A
+            WHERE A.st_distance_sphere < %s;
+            """
+            if print_sql:
+                print(sql_query)
+
+            try:
+                cursor.execute(sql_query, (str(point_longitude), str(point_latitude), str(distance)))
+            except psycopg2.DatabaseError as e:
+                print("DatabaseError: " + str(e))
+            else:
+                result = cursor.fetchall()
+                cursor.close()
+                postgresql_connection.close()
+                # Return the distance
+                return result
 
 
 def query_road_link_from_mongodb(link_id):
     """
     Query a road with link id stored in MongoDB
-    :param
-    link_id: string of two indexes (example: "3006-30069")
+    :param link_id: string of two indexes (example: "3006-30069")
     :return: records of the road from all time
     """
 
     client = MongoClient("127.0.0.1", 27017)
     db = client["traffic"]
     tsm_collection = db["traffic_speed_map"]
-    records = list(tsm_collection.find({"link_id": link_id}))
-    # records = list(tsm_collection.find().sort([('fetch_time', -1)]).limit(1))
+    road_records = list(tsm_collection.find({"link_id": link_id}))
+    # road_records = list(tsm_collection.find({"link_id": link_id}).sort([('capture_date', -1)]).limit(1))
     client.close()
 
-    if len(records) == 0:
+    if len(road_records) == 0:
         print("No traffic speed data of this road in current database")
         return None
     else:
-        return records
+        return road_records
+
+
+def update_latest_road_link_info_from_mongodb(link_list):
+    """
+    Query and update the latest information for the link list
+    :param link_list: a list of links queried from MongoDB
+    :return: an updated link list
+    """
+
+    client = MongoClient("127.0.0.1", 27017)
+    db = client["traffic"]
+    tsm_collection = db["traffic_speed_map"]
+
+    for link in link_list:
+        link_id = link['parent_id']
+        # Transfer the cursor to a list (TODO: May update according to the requirements)
+        latest_link_record = list(tsm_collection.find({"link_id": link_id}).sort([('capture_date_1970', -1)]).limit(1))
+        # print(latest_link_record[0])
+        link_saturation_level = latest_link_record[0]['road_saturation_level']
+        link_traffic_speed = int(latest_link_record[0]['traffic_speed'])
+        link.append(link_saturation_level)
+        link.append(link_traffic_speed)
+
+    client.close()
+
+    return link_list
 
 
 if __name__ == '__main__':
     osm_query = OSMQuery()
+    # distance = osm_query.query_point_distance_from_road(114.26, 22.33, 157424143)
+    # print("Nearest distance between (114.26, 22.33) and the road (way-id 157424143): " + str(distance) + "\n")
 
-    distance = osm_query.query_point_distance_from_road(114.26, 22.33, 157424143)
-    print("Nearest distance between (114.26, 22.33) and the road (way-id 157424143): " + str(distance) + "\n")
+    link_list = osm_query.query_osm_link_list_from_point(114.20, 22.34, 1000)
+    print("Links(link_id, parent_id, st_length, st_distance_sphere) from (114.20, 22.34) within 1000 meters: ")
+    for link in link_list:
+        print(link)
 
-    road_list = osm_query.query_road_list_from_point(114.26, 22.33, 500)
-    for road in road_list:
-        print(road)
+    link_list = update_latest_road_link_info_from_mongodb(link_list)
--- src/query/preprocess.py
+++ src/query/preprocess.py
@@ -3,6 +3,8 @@ from urllib.error import HTTPError, URLError
 import urllib.request
 import json
 from pymongo import MongoClient
+import psycopg2
+from psycopg2 import extras
 
 
 def get_tsm_link_node_info(csv_path):
@@ -61,7 +63,7 @@ def convert_coordinates(eastings, northings):
     """
 
     transform_path = 'http://www.geodetic.gov.hk/transform/v2/?inSys=hkgrid&outSys=wgsgeog&outUnit=decDeg' \
-                   '&e=' + eastings + '&n=' + northings
+                     '&e=' + eastings + '&n=' + northings
     try:
         response = urllib.request.urlopen(transform_path)
     except HTTPError as e:
@@ -89,9 +91,102 @@ def store_tsm_link_node_info(link_records, node_records):
     client.close()
 
 
-if __name__ == '__main__':
-    link_records, node_records = get_tsm_link_node_info('../../tsm_link_and_node_info_v2.csv')
-    # print(link_records)
-    # print(node_records)
-    store_tsm_link_node_info(link_records, node_records)
+def get_mongodb_osm_links():
+    """
+    Get osm links information from MongoDB
+    """
 
+    client = MongoClient('127.0.0.1', 27018)
+    db = client['local']
+    osm_link_collection = db['osm_links']
+    osm_links = list(osm_link_collection.find())
+    client.close()
+    return osm_links
+
+
+def process_osm_links(osm_links):
+    """
+    Process and organize the osm link result from MongoDB
+    :param osm_links: a list of osm links queried from MongoDB
+    :return: a list of processed dicts with link information
+    """
+
+    link_dict_list = []
+    for link in osm_links:
+        # print(link['path'])
+        parent_id = link['id']
+        # Build edges
+        for nid in range(len(link['path']) - 1):
+            start_node = link['path'][nid]
+            end_node = link['path'][nid + 1]
+            link_id = start_node['id'] + "-" + end_node['id']
+            # print(link_id)
+            start_node_long = str(start_node['coordinates'][0])
+            start_node_lat = str(start_node['coordinates'][1])
+            end_node_long = str(end_node['coordinates'][0])
+            end_node_lat = str(end_node['coordinates'][1])
+            line_string = 'LINESTRING(' + start_node_long + ' ' + start_node_lat + ', ' \
+                          + end_node_long + ' ' + end_node_lat + ')'
+            # print(line_string)
+            dict = {'link_id': link_id, 'parent_id': parent_id,
+                    'start_long': start_node_long, 'start_lat': start_node_lat,
+                    'end_long': end_node_long, 'end_lat': end_node_lat, 'line_string': line_string}
+            link_dict_list.append(dict)
+            print(dict)
+
+    return tuple(link_dict_list)
+
+
+def create_hk_osm_link_postgis_database(link_dicts):
+    """
+    Create hk_osm_link PostGIS database
+    :param link_dicts: dicts contain link information from MongoDB
+    :return:
+    """
+
+    try:
+        # Create connection to ices database
+        postgresql_connection = psycopg2.connect(dbname='gis', host='127.0.0.1', port='5432',
+                                                 user='postgres', password='manage')
+    except psycopg2.OperationalError as error:
+        print('OperationalError: ' + str(error))
+    else:
+        # Create a cursor object using dict output format
+        cursor = postgresql_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        create_database_sql = """
+        CREATE TABLE hk_osm_link (link_id text, parent_id text, 
+        start_long text, start_lat text, end_long text, end_lat text, link GEOMETRY(LINESTRING, 4326));
+        """
+
+        insert_sql = """
+        INSERT INTO hk_osm_link(link_id, parent_id, start_long, start_lat, end_long, end_lat, link) 
+        VALUES (%(link_id)s, %(parent_id)s, 
+        %(start_long)s, %(start_lat)s, %(end_long)s, %(end_lat)s, ST_GeomFromText(%(line_string)s, 4326));
+        """
+
+        create_index_sql = """
+        CREATE INDEX hk_osm_link_index ON hk_osm_link USING GIST (link);
+        """
+        try:
+            cursor.execute(create_database_sql)    # Create database
+            cursor.executemany(insert_sql, link_dicts)    # Insert osm links
+            cursor.execute(create_index_sql)    # Create index
+            postgresql_connection.commit()    # Commit the changes
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if postgresql_connection is not None:
+                cursor.close()
+                postgresql_connection.close()
+
+
+if __name__ == '__main__':
+    # Store TSM node and link info into MongoDB
+    # link_records, node_records = get_tsm_link_node_info('../../tsm_link_and_node_info_v2.csv')
+    # store_tsm_link_node_info(link_records, node_records)
+
+    # Store OSM links from TSM links into PostGis
+    osm_links = get_mongodb_osm_links()
+    link_dicts = process_osm_links(osm_links)
+    create_hk_osm_link_postgis_database(link_dicts)
